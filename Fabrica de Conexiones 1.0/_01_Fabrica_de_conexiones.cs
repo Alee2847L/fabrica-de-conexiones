9335b31232693eb0c44899d1442c4d0209360508
Leonel Ramos <Leonel Ramos@DESKTOP-RV9MRO8>
2019-05-27 12:43:03 -0600

    primeros cambios

---
+++ Fabrica de Conexiones 1.0/_01_Fabrica_de_conexiones.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrica_de_Conexiones_1._0
+{
+    class _01_Fabrica_de_conexiones
+    {
+    }
+}
